4b5a5e5eabd7b25199150b8989225ddac6ba4bb0
CaioLira18 <caiozao1212@gmail.com>
2025-10-25 00:24:01 -0300

    [feat] - Header Created

--- front/wweufc/src/App.jsx
+++ front/wweufc/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ front/wweufc/src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="logoContainer">
+        <img src="https://res.cloudinary.com/dthgw4q5d/image/upload/v1761361945/TKO_Group_Holdings_logo.svg_rfmskw.png" alt="" />
+      </div>
+      <div className="nav-links">
+        <div className="nav-link">
+            <ul>
+                <li>Home</li>
+            </ul>
+        </div>
+        <div className="nav-link">
+            <ul>
+                <li>Lutadores</li>
+            </ul>
+        </div>
+        <div className="nav-link">
+            <ul>
+                <li>Campeões</li>
+            </ul>
+        </div>
+        <div className="nav-link">
+            <ul>
+                <li>Proximos Eventos</li>
+            </ul>
+        </div>
+        <div className="nav-link">
+            <ul>
+                <li>WWE</li>
+            </ul>
+        </div>
+        <div className="nav-link">
+            <ul>
+                <li>UFC</li>
+            </ul>
+        </div> 
+      </div>
+    </div>
+  )
+}
+
+export default Header
