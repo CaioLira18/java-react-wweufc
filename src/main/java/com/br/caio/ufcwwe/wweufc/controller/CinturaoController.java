6dbf1040ab5ab8024aa5d64c00aed834a4ed019b
CaioLira18 <caiozao1212@gmail.com>
2025-10-25 00:04:40 -0300

    [feat] - Creating front

--- src/main/java/com/br/caio/ufcwwe/wweufc/controller/CinturaoController.java
+++ src/main/java/com/br/caio/ufcwwe/wweufc/controller/CinturaoController.java
@@ -5,16 +5,24 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.br.caio.ufcwwe.wweufc.entities.Cinturao;
 import com.br.caio.ufcwwe.wweufc.service.CinturaoService;
 
+@RestController
+@CrossOrigin(origins = {
+    "http://localhost:5173",
+})
+@RequestMapping("/api/cinturoes")
 public class CinturaoController {
     @Autowired
     private CinturaoService cinturaoService;
