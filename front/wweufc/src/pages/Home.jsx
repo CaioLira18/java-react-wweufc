d365d34a410affdbd32a55800544e7c334a0d5db
CaioLira18 <caiozao1212@gmail.com>
2025-10-25 00:51:30 -0300

    [feat] - Slide Home Created

--- front/wweufc/src/pages/Home.jsx
+++ front/wweufc/src/pages/Home.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      
+      <div className="slideContainer">
+        <div className="backgroundSlide">
+            <img src="https://res.cloudinary.com/dthgw4q5d/image/upload/v1761363166/1_igiwad.png" alt="" />
+        </div>
+        <div className="slideBox">
+            <h1>Alex Poatan derrota Ankalaev e conquista o cinturão dos pesos medios</h1>
+
+            <div className="buttonSlide">
+                <button>Ver Materia</button>
+            </div>
+        </div>
+
+      </div>
     </div>
   )
 }
