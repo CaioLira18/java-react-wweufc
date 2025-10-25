854ae149c4ee31702fed6ba76c0cfa0e044dd04a
CaioLira18 <caiozao1212@gmail.com>
2025-10-24 22:58:41 -0300

    first commit

---
+++ src/main/java/com/br/caio/ufcwwe/wweufc/wweufc.java
@@ -0,0 +1,13 @@
+package com.br.caio.ufcwwe.wweufc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class wweufc {
+
+	public static void main(String[] args) {
+		SpringApplication.run(wweufc.class, args);
+	}
+
+}
