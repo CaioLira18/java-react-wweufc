d51799d45fd4b10bbf016565e9c6e500c0b26bbe
CaioLira18 <caiozao1212@gmail.com>
2025-11-19 21:30:36 -0300

    [feat] - Update with empresa select

--- front/wweufc/src/components/Lutadores.jsx
+++ front/wweufc/src/components/Lutadores.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 const Lutadores = () => {
 
   const [lutadores, setLutadores] = useState([]);
-
+  const [empresa, setEmpresa] = useState("WWE");
   const API_URL = "http://localhost:8080/api";
 
   useEffect(() => {
@@ -23,13 +23,54 @@ const Lutadores = () => {
 
   return (
     <div>
+      <div className="empressSelect">
+        <img 
+          src="https://res.cloudinary.com/dthgw4q5d/image/upload/v1763598134/WWE_Logo.svg_b2gftp.png" 
+          alt="WWE" 
+          onClick={() => setEmpresa("WWE")}
+          style={{ cursor: 'pointer', opacity: empresa === "WWE" ? 1 : 0.5 }}
+        />
+        <img 
+          src="https://res.cloudinary.com/dthgw4q5d/image/upload/v1763598163/UFC_logo.svg_t3belb.png" 
+          alt="UFC" 
+          onClick={() => setEmpresa("UFC")}
+          style={{ cursor: 'pointer', opacity: empresa === "UFC" ? 1 : 0.5 }}
+        />
+      </div>
       <div className="lutadoresContainer">
-        <h1>Lutadores</h1>
+         <div className="rowLutadores">
+          <div className="informationFighters">
+            <h1>Campeões</h1>
+            <p>Mostrar Tudo</p>
+          </div>
+          {lutadores
+            .filter(lutador => lutador.type === 'CHAMPION' && lutador.empress === empresa)
+            .map((lutador) => (
+              <div className="lutadoresBox" key={lutador.id}>
+                <div className="informarionsBox">
+                  <div className="imageLutador">
+                    <img 
+                      src={lutador.imageRender} 
+                      alt={`Render do Lutador ${lutador.name}`} 
+                    />
+                  </div>
+
+                  <div className="informationLutador">
+                    <h2>{lutador.name}</h2>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+        </div>
 
         <div className="rowLutadores">
-          <h1>Campeões</h1>
+          <div className="informationFighters">
+            <h1>Lutadores</h1>
+            <p>Mostrar Tudo</p>
+          </div>
           {lutadores
-            .filter(lutador => lutador.type === 'CHAMPION')
+            .filter(lutador => lutador.type === 'NONE' && lutador.empress === empresa)
             .map((lutador) => (
               <div className="lutadoresBox" key={lutador.id}>
                 <div className="informarionsBox">
